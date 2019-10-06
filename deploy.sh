c99076f8e779d8d813f199d4818d93b817bcbea1
Ivan Stanojevic <ivanstan@gmail.com>
2019-10-06 12:34:45 +0200

    release-201910061234

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
-git checkout master
 yarn build
-git add build
+
+git add docs
 git commit -m "release"
-git subtree split --prefix build -b gh-pages # create a local gh-pages branch containing the splitted output folder
-#git push origin --delete gh-pages
-git push -f origin gh-pages:gh-pages # force the push of the gh-pages branch to the remote gh-pages branch at origin
-#git branch -d gh-pages # delete the local gh-pages because you will need it: ref
-#rm -rf build
+git push
---
+++ docs/precache-manifest.d8d88a46accb2678baf4e54fe1545b89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39a914e108fd9cfaf06ae2da7145e102",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3b1c3d78372d6d7d59e",
+    "url": "/static/css/2.5bc09813.chunk.css"
+  },
+  {
+    "revision": "d686fa0c443b0305d8fd",
+    "url": "/static/css/main.8d6982f6.chunk.css"
+  },
+  {
+    "revision": "e3b1c3d78372d6d7d59e",
+    "url": "/static/js/2.b3455904.chunk.js"
+  },
+  {
+    "revision": "d686fa0c443b0305d8fd",
+    "url": "/static/js/main.3071750d.chunk.js"
+  },
+  {
+    "revision": "5d4172d5b547a58f2f0c",
+    "url": "/static/js/runtime~main.ab2510d2.js"
+  }
+]);
