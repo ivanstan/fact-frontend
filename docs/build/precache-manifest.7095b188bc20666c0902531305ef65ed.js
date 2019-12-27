f22155d5f0ba790f624c552b36568af8d916d1a7
Ivan Stanojevic <ivan.stanojevic@protonmail.com>
2019-12-27 23:03:28 +0100

    release-201912272303

---
+++ docs/build/precache-manifest.7095b188bc20666c0902531305ef65ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa36d2a7697047b9989fee1826ca84b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc66f4b0626d822615e6",
+    "url": "/static/css/2.5bc09813.chunk.css"
+  },
+  {
+    "revision": "9af82ffbf79435f09233",
+    "url": "/static/css/main.5df2d20e.chunk.css"
+  },
+  {
+    "revision": "fc66f4b0626d822615e6",
+    "url": "/static/js/2.7765e4b6.chunk.js"
+  },
+  {
+    "revision": "9af82ffbf79435f09233",
+    "url": "/static/js/main.1faa3067.chunk.js"
+  },
+  {
+    "revision": "5d4172d5b547a58f2f0c",
+    "url": "/static/js/runtime~main.ab2510d2.js"
+  }
+]);
